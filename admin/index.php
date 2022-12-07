aa8d8fbd87d9f20bbda0e8ef8ec2460be773d2e4
meilinda.zheng <meilinda.zheng@usenobi.com>
2022-12-07 19:38:12 +0700

    adding admin panel using static interface (not connected to db yet)

--- admin/index.php
+++ admin/index.php
@@ -7,8 +7,90 @@
     <title>Admin Dashboard</title>
     <!-- Bootstrap CSS Link -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <!-- Link CSS -->
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
+    <div class="container-fluid p-0">
+        <!-- first content -->
+            <nav class="navbar navbar-expand-lg navbar-fixed-top" style="background-color: #344055;">
+                <div class="container-fluid">
+                    <img src="../images/logo.png" alt="" class="logo">
+                    <nav class="navbar navbar-expand-lg">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="" class="nav-link text-white">Welcome Guest</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </nav>
+        <!-- second content -->
+        <div style="background-color: #f2f2f3 ;">
+            <h4 class="text-center">Manage Platform</h4>
+        </div>
+        <!-- third content -->
+        <div class="row">
+            <div class="col-md-12 p-2">
+                <div class="text-center">
+                    <button class="btn btn-sm"  style="background-color: #344055;" style="background-color: #344055;">
+                        <a href="" class="btn text-white">
+                            Insert Products
+                        </a>
+                    </button>
+                    <button class="btn btn-sm"  style="background-color: #344055;">
+                        <a href="" class="btn text-white">
+                            View Products
+                        </a>
+                    </button>
+                    <button class="btn btn-sm"  style="background-color: #344055;">
+                        <a href="" class="btn text-white ">
+                            Insert Categories
+                        </a>
+                    </button>
+                    <button class="btn btn-sm"  style="background-color: #344055;">
+                        <a href="" class="btn text-white">
+                            View Categories
+                        </a>
+                    </button>
+                    <button class="btn btn-sm"  style="background-color: #344055;">
+                        <a href="" class="btn text-white">
+                            Insert Brand
+                        </a>
+                    </button>
+                    <button class="btn btn-sm"  style="background-color: #344055;">
+                        <a href="" class="btn text-white">
+                            View Brand
+                        </a>
+                    </button>
+                    <button class="btn btn-sm"  style="background-color: #344055;">
+                        <a href="" class="btn text-white">
+                            All Orders
+                        </a>
+                    </button>
+                    <button class="btn btn-sm"  style="background-color: #344055;">
+                        <a href="" class="btn text-white">
+                            All Payment
+                        </a>
+                    </button>
+                    <button class="btn btn-sm"  style="background-color: #344055;">
+                        <a href="" class="btn text-white">
+                            List Users
+                        </a>
+                    </button>
+                    <button class="btn btn-sm"  style="background-color: #344055;">
+                        <a href="" class="btn text-white">
+                            Admin Logout
+                        </a>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Last Content -->
+    <div class="p-1" style="background-color: #eeeee4; position: absolute; bottom:0; width:100%">
+        <p class="text-center">&copy; Meilinda & Keyko</p>
+    </div>
     <!-- Bootstrap JS Link -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
