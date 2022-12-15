7b1e39184b53f757d2c5cbb33a16e05728dadb30
meilinda.zheng <meilinda.zheng@usenobi.com>
2022-12-15 20:43:30 +0700

    inserting user registration data connecting to database

--- users_area/user_registration.php
+++ users_area/user_registration.php
@@ -1,3 +1,7 @@
+<?php
+    include('../includes/connect.php');
+    include('../functions/common_function.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -56,3 +60,26 @@
     </div>
 </body>
 </html>
+
+<!-- php logic -->
+<?php
+    if(isset($_POST['user_register'])){
+        $user_username = $_POST['user_username'];
+        $user_email = $_POST['user_email'];
+        $user_password = $_POST['user_password'];
+        $conf_user_password = $_POST['conf_user_password'];
+        $user_address = $_POST['user_address'];
+        $user_contact = $_POST['user_contact'];
+        $user_ip = getIPAddress();
+
+        $insert_query = "INSERT INTO `user_table` 
+        VALUES('','$user_username', '$user_email','$user_password','$user_ip','$user_address','$user_contact')";
+        $sql_execute = mysqli_query($conn,$insert_query);
+        if($sql_execute){
+            echo "<script>alert('User Inserted')</script>";
+        }else{
+            die(mysqli_error($conn));
+        }
+    }
+
+?>
